c80d87781030c588df8f20b7004884357d713820
mydubhe <mydubhe@sina.com>
2016-12-12 23:07:11 +0800

    first commit

---
+++ DubheVideoCapture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DubheVideoCapture
+//
+//  Created by dubhe on 16/12/12.
+//  Copyright © 2016年 dubhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
