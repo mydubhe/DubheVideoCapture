87308f7042f936843a73a441ed616016cc5773db
mydubhe <mydubhe@sina.com>
2016-12-12 23:13:31 +0800

    pod install

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '8.0'
+
+target 'DubheVideoCapture' do
+	pod 'AFNetworking', '~>2.0'
+end 
---
+++ Pods/Target Support Files/Pods-DubheVideoCapture/Pods-DubheVideoCapture-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DubheVideoCapture : NSObject
+@end
+@implementation PodsDummy_Pods_DubheVideoCapture
+@end
